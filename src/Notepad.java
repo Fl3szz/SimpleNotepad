74f7196e993ca76358e45e12b67572efac532f0d
Dominik <dominik.niedospial1222@gmail.com>
2023-12-21 12:26:07 +0100

    Status bar for tracking lines and columns has been added

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,13 +25,15 @@ public class Notepad extends JFrame implements ActionListener {
 
         JMenuItem darkMode;
         JMenuItem lightMode;
+        JLabel statusLabel;
 
     Notepad() {
+             //setting up the frame
             this.setLayout(new BorderLayout());
             this.setSize(1000,750);
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setLocationRelativeTo(null);
-
+            //creating menus and menu bar
             menuBar = new JMenuBar();
             icon = new ImageIcon("C:\\Users\\Admin\\Desktop\\note.jpg");
             Image image = icon.getImage();
@@ -45,7 +50,8 @@ public class Notepad extends JFrame implements ActionListener {
             darkMode = new JMenuItem("Dark mode");
             lightMode = new JMenuItem("Light mode");
             lineWrap = new JCheckBoxMenuItem ("Line Wrap",true);
-
+            statusLabel = new JLabel("Lines: 1, Columns: 1");//Status label
+            //adding ActionListener to items
             loadItem.addActionListener(this);
             saveItem.addActionListener(this);
             exitItem.addActionListener(this);
@@ -53,7 +59,7 @@ public class Notepad extends JFrame implements ActionListener {
             darkMode.addActionListener(this);
             lightMode.addActionListener(this);
             lineWrap.addActionListener(this);
-
+            //Keyboard shortcuts
             loadItem.setMnemonic('l'); //L for load
             saveItem.setMnemonic('s');//S for save
             exitItem.setMnemonic('e');//E for exit
@@ -61,7 +67,7 @@ public class Notepad extends JFrame implements ActionListener {
             darkMode.setMnemonic('n');//n dark mode
             lightMode.setMnemonic('f');//f for light mode
             lineWrap.setMnemonic('w');//w to enable line wrapping
-
+            //adding items to menus
             fileMenu.add(loadItem);
             fileMenu.add(saveItem);
             fileMenu.add(exitItem);
@@ -81,14 +87,42 @@ public class Notepad extends JFrame implements ActionListener {
 
             this.setIconImage(image);
             this.setJMenuBar(menuBar);
-
+            this.add(statusLabel, BorderLayout.SOUTH);
             textArea = new JTextArea();
             textArea.setBackground(Color.white);
             JScrollPane scrollPane = new JScrollPane(textArea);
             this.add(scrollPane, BorderLayout.CENTER);
             this.setTitle("Notepad");
             this.setVisible(true);
+            //added Document listener for tracking changes in text area
+            textArea.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    try {
+                        updateStatusBar();
+                    } catch (BadLocationException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
 
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    try {
+                        updateStatusBar();
+                    } catch (BadLocationException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+                    try {
+                        updateStatusBar();
+                    } catch (BadLocationException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
         }
     @Override
         public void actionPerformed(ActionEvent e) {
@@ -155,5 +189,11 @@ public class Notepad extends JFrame implements ActionListener {
                 textArea.setForeground(Color.black);
                 this.setBackground(Color.white);
             }
+        }
+        //method that updates the bar whenever you write something
+        private void updateStatusBar() throws BadLocationException {
+        int lines = textArea.getLineCount();
+        int columns = textArea.getCaretPosition() - textArea.getLineStartOffset(textArea.getLineCount()-1);
+        statusLabel.setText("Lines"+lines+" ,Columns:"+columns);
         }
 }
